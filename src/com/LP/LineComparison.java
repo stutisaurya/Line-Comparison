540e74869b87b621c79963b4740c91765a00aadd
stutisaurya <stuti.saurya@gmail.com>
2021-06-21 20:13:58 +0530

    Added: Check Equal or Not

--- src/com/LP/LineComparison.java
+++ src/com/LP/LineComparison.java
@@ -3,15 +3,18 @@ package com.LP;
 import java.util.Scanner;
 
 class LineComparison {
-
+	// variables
 	int x1, y1, x2, y2;
-	int ans = 0;
+	int ans;
 
-	void welcome() {
+	void welcome()// method to print welcome message
+	{
 		System.out.println("Welcome to empwage computation");
 	}
 
-	void calculate() {
+	public Double calculate()
+	// use_case 1 (method to calculate length of line using Cartesian system)
+	{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter x1, for line : ");
 		int x1 = sc.nextInt();
@@ -21,17 +24,27 @@ class LineComparison {
 		int x2 = sc.nextInt();
 		System.out.print("Enter y2, for line1 : ");
 		int y2 = sc.nextInt();
-		double ans = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println("length of line is:" + ans);
+		Double ans = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println("length is" + ans);
+		return ans;
 	}
 
 	public static void main(String args[]) {
-
 		LineComparison line1 = new LineComparison();
 		line1.welcome();
 		System.out.println("coordinates of line 1 are");
-		line1.calculate();
+		Double length1 = line1.calculate();// calculate length of first line
 
+		System.out.println("coordinates of line 2 are");
+		LineComparison line2 = new LineComparison();
+		Double length2 = line2.calculate();// calculate length of second line
+		boolean a = length1.equals(length2);
+		System.out.println("" + a);// UseCase 2 check the equality of two line
+		if (a == true)
+			System.out.println("Two lines are equal");
+		else {
+			System.out.println("Two lines are  not equal");
+		}
 	}
 
 }
